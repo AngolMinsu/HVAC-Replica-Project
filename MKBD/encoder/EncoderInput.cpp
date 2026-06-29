74d606bfe44c66baa85b9269e8b8897b040323b0
Angol Minsu <steinsgate888@gmail.com>
2026-06-29 23:06:12 +0900

    Modify : 엔코더의 HW문제로 인해 4step 샘플링 -> 전압 변경으로 변경

--- MKBD/encoder/EncoderInput.cpp
+++ MKBD/encoder/EncoderInput.cpp
@@ -4,19 +4,12 @@ static uint8_t isEncoderFallingEdge(uint8_t previousLevel, uint8_t currentLevel)
   return previousLevel == HIGH && currentLevel == LOW;
 }
 
-static uint8_t makeEncoderState(uint8_t a, uint8_t b) {
-  return ((a == LOW) ? 0x02 : 0x00) | ((b == LOW) ? 0x01 : 0x00);
+static uint8_t driverDirectionFromB(uint8_t bLevel) {
+  return (bLevel == HIGH) ? ENCODER_EVENT_DRIVER_CW : ENCODER_EVENT_DRIVER_CCW;
 }
 
-static int8_t decodeEncoderTransition(uint8_t previousState, uint8_t currentState) {
-  static const int8_t table[16] = {
-     0,  1, -1,  0,
-    -1,  0,  0,  1,
-     1,  0,  0, -1,
-     0, -1,  1,  0
-  };
-
-  return table[(previousState << 2) | currentState];
+static uint8_t passengerDirectionFromB(uint8_t bLevel) {
+  return (bLevel == HIGH) ? ENCODER_EVENT_PASSENGER_CW : ENCODER_EVENT_PASSENGER_CCW;
 }
 
 void initEncoderHistory(EncoderHistory& history) {
@@ -39,28 +32,10 @@ uint8_t detectEncoderEvent(EncoderHistory& history, const EncoderLevels& levels,
       event = ENCODER_EVENT_DRIVER_SW;
     } else if (isEncoderFallingEdge(history.prevPassengerSw, levels.passengerSw)) {
       event = ENCODER_EVENT_PASSENGER_SW;
-    } else {
-      uint8_t prevDriverState = makeEncoderState(history.prevDriverA, history.prevDriverB);
-      uint8_t currDriverState = makeEncoderState(levels.driverA, levels.driverB);
-      uint8_t prevPassengerState = makeEncoderState(history.prevPassengerA, history.prevPassengerB);
-      uint8_t currPassengerState = makeEncoderState(levels.passengerA, levels.passengerB);
-
-      history.driverStep += decodeEncoderTransition(prevDriverState, currDriverState);
-      history.passengerStep += decodeEncoderTransition(prevPassengerState, currPassengerState);
-
-      if (history.driverStep >= 4) {
-        history.driverStep = 0;
-        event = ENCODER_EVENT_DRIVER_CW;
-      } else if (history.driverStep <= -4) {
-        history.driverStep = 0;
-        event = ENCODER_EVENT_DRIVER_CCW;
-      } else if (history.passengerStep >= 4) {
-        history.passengerStep = 0;
-        event = ENCODER_EVENT_PASSENGER_CW;
-      } else if (history.passengerStep <= -4) {
-        history.passengerStep = 0;
-        event = ENCODER_EVENT_PASSENGER_CCW;
-      }
+    } else if (isEncoderFallingEdge(history.prevDriverA, levels.driverA)) {
+      event = driverDirectionFromB(levels.driverB);
+    } else if (isEncoderFallingEdge(history.prevPassengerA, levels.passengerA)) {
+      event = passengerDirectionFromB(levels.passengerB);
     }
 
     if (event != ENCODER_EVENT_NONE) {
